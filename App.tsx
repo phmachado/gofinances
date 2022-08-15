3b97dbe900612902545775591375865a2ae6214b
Pedro Machado <pedro.machado@liferay.com>
2022-08-15 15:21:14 -0300

    Chapter II | Using SplashScreen and custom fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 import theme from "./src/global/styles/theme";
 import Dashboard from "./src/screens/Dashboard";
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    const showSplashScreen = async () => {
+      await SplashScreen.preventAutoHideAsync();
+    };
+
+    showSplashScreen();
+  }, []);
+
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      await SplashScreen.hideAsync();
+    };
+
+    if (isLoaded) hideSplashScreen();
+  }, [isLoaded]);
+
+  if (!isLoaded) return null;
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
