0221b009ca874829c35ba270fba86092a498294a
Pedro Machado <pedro.machado@liferay.com>
2022-08-18 10:23:25 -0300

    Chapter II | Transaction type component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container, Header, Title, Form, Fields } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypes,
+} from "./styles";
 import Input from "../../components/Forms/Input";
 import Button from "../../components/Forms/Button";
+import TransactionTypeButton from "../../components/Forms/TransactionTypeButton";
 
 export default function Register() {
+  const [transactionType, setTransactionType] = useState("");
+
+  function handleTransactionTypeSelect(type: "up" | "down") {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -14,6 +28,20 @@ export default function Register() {
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionTypeSelect("up")}
+              isActive={transactionType === "up"}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionTypeSelect("down")}
+              isActive={transactionType === "down"}
+            />
+          </TransactionTypes>
         </Fields>
         <Button title="Enviar" />
       </Form>
