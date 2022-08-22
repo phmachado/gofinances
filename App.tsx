46463c497e5edd64767311642c00ae9b2a201f86
Pedro Machado <pedro.machado@liferay.com>
2022-08-22 15:48:11 -0300

    Chapter II | Tab navigation

--- App.tsx
+++ App.tsx
@@ -7,9 +7,10 @@ import {
   Poppins_500Medium,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
+import { NavigationContainer } from "@react-navigation/native";
 
+import AppRoutes from "./src/routes/app.routes";
 import theme from "./src/global/styles/theme";
-import Register from "./src/screens/Register";
 
 export default function App() {
   const [isLoaded] = useFonts({
@@ -38,7 +39,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
